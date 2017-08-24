01d3b01f5870a444fb18d54893f82f9adf072d0a
Cristina Salvo <csalvo@spendsetter.com>
2017-08-24 11:26:33 -0400

    initial files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'khv1OURTDzM99WAT4a0fqgJpi',
+  consumer_secret: '00DhWkCzZQ4Ad2XVpzXl3XmX3XCq5Qjm3qW4FaOgltrpr7MEHL',
+  access_token_key: '900732465786085377-gz6NMAdoiEQ61JtWbW6b4WKKipBI1kV',
+  access_token_secret: '9lYp9BAMREUus0dSxKl31nZUVE8kY2h8Pqt4XpheDjXMR',
+}
