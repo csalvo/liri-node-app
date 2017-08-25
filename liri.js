b23ff5b58f3b6cf607f0d73b8a671caaad0d01ca
Cristina Salvo <csalvo@spendsetter.com>
2017-08-25 15:12:23 -0400

    spotify logic

--- liri.js
+++ liri.js
@@ -9,7 +9,7 @@ switch (process.argv[2]) {
     case "my-tweets":
         myTweets();
     case "spotify-this-song":
-    	spotifyStuff();
+        spotifyStuff();
 }
 
 
@@ -35,13 +35,39 @@ function myTweets() {
 
 //spotify
 function spotifyStuff() {
-	spotClient.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data.tracks.items[0]); 
-});
 
+    var userQuery = process.argv[3];
+    if (userQuery) {
+        for (var i = 4; i < process.argv.length; i++) {
+            userQuery = userQuery + " " + process.argv[i];
+        }
+    } else {
+        userQuery = "The Sign";
+    }
 
+    console.log("Here is info for the song: " + userQuery);
+    console.log("--------------------------------------------");
+
+    spotClient.search({ type: 'track', query: userQuery }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        //go back and make this case insensitive
+        for (var i = 0; i < data.tracks.items.length; i++) {
+            if (userQuery === data.tracks.items[i].name) {
+                console.log("Artist: " + data.tracks.items[i].artists[0].name);
+                console.log("Song title: " + data.tracks.items[i].name);
+                console.log("Album: " + data.tracks.items[i].album.name);
+                if (data.tracks.items[i].preview_url != null) {
+                    console.log("Preview: " + data.tracks.items[i].preview_url);
+                } else {
+                    console.log("No preview available.")
+                }
+                console.log("-------------------------------");
+
+            }
+
+        }
+    });
 }
